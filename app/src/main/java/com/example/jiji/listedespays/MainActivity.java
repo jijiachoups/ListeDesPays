e8aad1e599c825fd82fd548fb59b2ec3312e09d5
jijiachoups <nejia.matmati@gmail.com>
2017-03-12 13:31:53 +0100

    ok

---
+++ app/src/main/java/com/example/jiji/listedespays/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.jiji.listedespays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView ;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Tunisie",
+                "Argentine",
+                "Brésil",
+                "Cambodge",
+                "Colombie",
+                "Gabon",
+                "Mali",
+                "Zimbabwe"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                      itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+    }
+
+
+
+}
